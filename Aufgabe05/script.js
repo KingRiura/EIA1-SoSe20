f8c0260915a3a84e6c82a35d715999a9b711ffd1
KingRiura <64391411+KingRiura@users.noreply.github.com>
2020-06-01 21:12:19 +0200

    corrected values

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -1,8 +1,13 @@
 "use strict";
-var welt = 29016;
+
+//document.getElementById('eu2018').innerHTML = europa2018;
+//document.getElementById('eu2').innerHTML = europa2;
+
+var welt = 31116.5;
+//Europe
 var europa2018 = 4209.3;
 var europa2 = europa2018 * 100 / welt;
-var europa2008 = 4965.75;
+var europa2008 = 4965.7;
 var europa3 = europa2018 / europa2008 - 1;
 var europa4 = europa2018 - europa2008;
 console.group('Europa');
@@ -11,6 +16,8 @@ console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit " + eur
 console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + europa3 + "% verändert.");
 console.log("2018 im Vergleich zu 2008 sind das " + europa4 + "kg CO2.");
 console.groupEnd();
+
+//Africa
 var afrika2018 = 1235.5;
 var afrika2 = afrika2018 * 100 / welt;
 var afrika2008 = 1028;
@@ -22,6 +29,8 @@ console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit " + afr
 console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um " + afrika3 + "% verändert.");
 console.log("2018 im Vergleich zu 2008 sind das " + afrika4 + "k g CO2.");
 console.groupEnd();
+
+//NA
 var nordamerika2018 = 6035.6;
 var nordamerika2 = nordamerika2018 * 100 / welt;
 var nordamerika2008 = 6600.4;
@@ -33,6 +42,8 @@ console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " 
 console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + nordamerika3 + "% verändert.");
 console.log("2018 im Vergleich zu 2008 sind das " + nordamerika4 + "kg CO2.");
 console.groupEnd();
+
+//SA
 var südamerika2018 = 1261.5;
 var südamerika2 = südamerika2018 * 100 / welt;
 var südamerika2008 = 1132.6;
@@ -44,14 +55,29 @@ console.log("Relativ zur Gesamtemission der Welt verursacht Südamerika damit " 
 console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + südamerika3 + "% verändert.");
 console.log("2018 im Vergleich zu 2008 sind das " + südamerika4 + "kg CO2.");
 console.groupEnd();
+
+//Asia
 var asien2018 = 16274.1;
 var asien2 = asien2018 * 100 / welt;
 var asien2008 = 12954.7;
 var asien3 = asien2018 / asien2008 - 1;
 var asien4 = asien2018 - asien2008;
-console.group('Asiena');
+console.group('Asien');
 console.log("Die Emission von Asien ist " + asien2018 + "kg CO2.");
 console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit " + asien2 + "%.");
 console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um " + asien3 + "% verändert.");
 console.log("2018 im Vergleich zu 2008 sind das " + asien4 + "kg CO2.");
+console.groupEnd();
+
+//Australia
+var australien2018 = 2100.5;
+var australien2 = australien2018 * 100 / welt;
+var australien2008 = 1993;
+var australien3 = australien2018 / aaustralien2008 - 1;
+var australien4 = australien2018 - australien2008;
+console.group('Australien');
+console.log("Die Emission von Australien ist " + australien2018 + "kg CO2.");
+console.log("Relativ zur Gesamtemission der Welt verursacht Australien damit " + australien2 + "%.");
+console.log("Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um " + australien3 + "% verändert.");
+console.log("2018 im Vergleich zu 2008 sind das " + australien4 + "kg CO2.");
 console.groupEnd();
